c385f7c0d8c04e7d0e2db85bfc11e7334c59504a
Harry Flanagan <S00189591@mail.itsligo.ie>
2019-12-11 10:25:39 +0000

    Created Players

--- XmasExam_HarryFlanagan/MainWindow.xaml.cs
+++ XmasExam_HarryFlanagan/MainWindow.xaml.cs
@@ -20,9 +20,83 @@ namespace XmasExam_HarryFlanagan
     /// </summary>
     public partial class MainWindow : Window
     {
+        /*Lists*/
+        List<Player> allPlayers = new List<Player>();
+
+        //Name Arrays
+        string[] firstNames = {
+                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+
+
+        string[] lastNames = {
+                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+            };
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*Create Players*/
+            CreateRandomPlayer();
+            CreatePlayers();
+
+           
+           
+
+
+      
+        }
+        private void CreateRandomPlayer()
+        {
+            // Create a Random object  
+            Random rand = new Random();
+            // Generate a random index less than the size of the array.  
+            int fNameIndex = rand.Next(firstNames.Length);
+            // Generate a random index less than the size of the array.  
+            int lNameIndex = rand.Next(lastNames.Length);
+
+            DateTime RandomDay()
+            {
+                DateTime start = new DateTime(1990, 1, 1);
+                int range = (DateTime.Today - start).Days;
+                return start.AddDays(rand.Next(range));
+            }
+
+            Player p1 = new Player(firstNames[fNameIndex], lastNames[lNameIndex], Position.GoalKeeper, RandomDay());
+
+            /*Add Player to List*/
+            allPlayers.Add(p1);
+        }
+        public void CreatePlayers()
+        {
+            Player p2 = new Player("Sophie", "O'Neill", Position.GoalKeeper, new DateTime(1993, 06, 03));
+            Player p3 = new Player("Grace", "Walsh", Position.GoalKeeper, new DateTime(1992, 07, 03));
+            Player p4 = new Player("Harry", "Lynch", Position.Defender, new DateTime(1999, 08, 03));
+            Player p5 = new Player("Luke", "Browne", Position.Defender, new DateTime(1998, 09, 03));
+            Player p6 = new Player("Simon", "Thomas", Position.Defender, new DateTime(1991, 10, 03));
+            Player p7 = new Player("Jack", "Hull", Position.Defender, new DateTime(1997, 11, 03));
+            Player p8 = new Player("Abigail", "Baker", Position.Defender, new DateTime(2000, 11, 03));
+            Player p9 = new Player("Shaina", "McGovern", Position.Defender, new DateTime(1989, 11, 03));
+            Player p10 = new Player("Jamie", "Flanagan", Position.Midfielder, new DateTime(1990, 11, 03));
+            Player p11 = new Player("James", "Collet", Position.Midfielder, new DateTime(1994, 02, 03));
+            Player p12 = new Player("Gary", "Eaton", Position.Midfielder, new DateTime(1995, 03, 03));
+            Player p13 = new Player("Paige", "Smith", Position.Midfielder, new DateTime(1996, 04, 03));
+            Player p14 = new Player("Jessica", "Carter", Position.Midfielder, new DateTime(1990, 05, 03));
+            Player p15 = new Player("Evan", "Doherty", Position.Midfielder, new DateTime(1999, 05, 03));
+            Player p16 = new Player("Peter", "Bell", Position.Forward, new DateTime(1999, 05, 03));
+            Player p17 = new Player("Harrison", "Ford", Position.Forward, new DateTime(1999, 10, 13));
+            Player p18 = new Player("Vicotor", "Emmanual", Position.Forward, new DateTime(1997, 05, 10));
+            Player p19 = new Player("Suzy", "Smith", Position.Forward, new DateTime(1996, 06, 03));
+
+            /*Add Player to List*/
+
+        }
+
     }
 }
---
+++ XmasExam_HarryFlanagan/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/***Author: Harry Flanagan***/
+/***Date: 11/12/2019***/
+
+namespace XmasExam_HarryFlanagan
+{
+    /*Enums*/
+    public enum Position
+    {
+        GoalKeeper,
+        Defender,
+        Midfielder,
+        Forward
+    }
+    class Player
+    {
+
+
+        /*Properties*/
+        public string  FirstName { get; set; }
+        public string Surname { get; set; }
+        public Position PreferedPosistion { get; set; }
+        public  DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+
+        /*Constructor*/
+        public Player(string firstName, string surname, Position preferedPostion, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            Surname = surname;
+            PreferedPosistion = preferedPostion;
+            DateOfBirth = dateOfBirth;
+        }
+    
+    }
+    /*Methods*/
+    public override
+
+}
